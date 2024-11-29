8b5e840132054612dd377cb69f51b99a7d2c2fad
Its1st <Newaction3@outlook.com>
2024-11-29 11:45:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7216182705:AAFdGJlXFmm5kh1b4toNk022c0RymViKOno")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@PsychoVillain19")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1163610781")
+
+
+
+
+
+
+
+  
